a051caa1d771a0014b4e5108df4198383513fcfc
Pratham <prathamsharmafaangm@gmail.com>
2024-03-31 15:30:14 +0530

    woring auth for jwt user

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <BrowserRouter >
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
+}
